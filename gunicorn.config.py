1a50429139086c61d0a072873192fcd75a852eac
evensyell <qingxi2158@gmail.com>
2020-03-19 10:47:39 +0800

    1

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -3,8 +3,8 @@ import multiprocessing
 bind = "127.0.0.1:8000"  # 绑定的ip与端口
 workers = 2  # 核心数
 threads = 16
-errorlog = "/home/backend/errorlog/gunicorn.error.log"  # 发生错误时log的路径
-accesslog = "/home/backend/accesslog/gunicorn.access.log"  # 正常时的log路径
+errorlog = "/home/log/gunicorn.error.log"  # 发生错误时log的路径
+# accesslog = "/home/backend/accesslog/gunicorn.access.log"  # 正常时的log路径
 # loglevel = 'debug'   #日志等级
 proc_name = "dj"  # 进程名
 
